e3df6801f5b11bae556c4e1bd0ba6ec45f339b67
Keith A. Lewis <keithalewis@live.com>
2016-02-15 11:54:59 -0500

    xll_alert_level talks to registry

--- xll/error.cpp
+++ xll/error.cpp
@@ -1,32 +1,115 @@
 #pragma warning(disable: 4996)
 #include "error.h"
 #include <Windows.h>
+#include <stdexcept>
+#include <string>
 
-int xll_alert_level(int set = 0)
-{
-	static int level = 0;
+struct RegKey {
+	HKEY hkey, h;
+	std::basic_string<TCHAR> subkey;
+	REGSAM sam;
+	DWORD disposition;
 
-	if (set != 0) {
-		// write to registry
-		level = set;
+	RegKey(HKEY hkey, LPCTSTR subkey, REGSAM sam = KEY_ALL_ACCESS)
+		: hkey(hkey), h(0), subkey(subkey), sam(sam)
+	{ }
+	RegKey(const RegKey&) = default;
+	RegKey& operator=(const RegKey&) = default;
+	~RegKey()
+	{
+		Close();
 	}
-	else if (level == 0) {
-		// read from registry
-		level = XLL_ALERT_ERROR|XLL_ALERT_WARNING|XLL_ALERT_INFO;
+
+	void Close()
+	{
+		if (h)
+			RegCloseKey(h);
+		h = 0;
 	}
 
-	return level;
-}
+	LONG Create()
+	{
+		return RegCreateKeyEx(hkey, subkey.c_str(), 0, 0, 0, sam, 0, &h, &disposition);
+	}
+	LONG Open()
+	{
+		return RegOpenKeyEx(hkey, subkey.c_str(), 0, sam, &h);
+	}
+	LONG QueryValue(LPCTSTR name, LPDWORD type, LPBYTE data, LPDWORD cbdata) 
+	{
+		return RegQueryValueEx(hkey, name, 0, type, data, cbdata);
+	}
+	LONG QueryValue(LPCTSTR name, LPDWORD value)
+	{
+		DWORD type;
+		LONG ret = QueryValue(name, &type, 0, 0);
+		if (ret != ERROR_SUCCESS)
+			return ret;
+		if (type != REG_DWORD)
+			return ERROR_INVALID_PARAMETER;
+
+		DWORD size;
+		return QueryValue(name, &type, (LPBYTE)value, &size);
+	}
+	LONG SetValue(LPCTSTR name, DWORD type, LPCVOID data, DWORD cbdata)
+	{
+		return RegSetKeyValue(hkey, subkey.c_str(), name, type, data, cbdata);
+	}
+	LONG SetValue(LPCTSTR name, DWORD value)
+	{
+		return SetValue(name, REG_DWORD, (LPCVOID)&value, sizeof(DWORD));
+	}
+};
+
+struct xll_alert {
+	DWORD level;
+	xll_alert()
+		: level((DWORD)~0)
+	{
+		RegKey rk(HKEY_CURRENT_USER, TEXT("KALX\\xll"), KEY_QUERY_VALUE|KEY_SET_VALUE|KEY_READ);
+		if (ERROR_SUCCESS != rk.Create()) {
+			XLL_ERROR("create key failed: KALX\\xll", true);
+			return;
+		}
+		if (rk.disposition == REG_CREATED_NEW_KEY) {
+			level = XLL_ALERT_ERROR|XLL_ALERT_WARNING|XLL_ALERT_INFO;
+			if (ERROR_SUCCESS != rk.SetValue(TEXT("xll_alert_level"), level)) {
+				level = (DWORD)~0;
+				return;
+			}
+		}
+		else {
+			if (ERROR_SUCCESS != rk.QueryValue(TEXT("xll_alert_level"), &level)) {
+				level = (DWORD)~0;
+				return;
+			}
+		}
+	}
+	void set(DWORD _level)
+	{
+		RegKey rk(HKEY_CURRENT_USER, TEXT("KALX\\xll"), KEY_SET_VALUE);
+		if (ERROR_SUCCESS != rk.Open()) {
+			XLL_ERROR("Open key failed: KALX\\xll", true);
+			return;
+		}
+		if (ERROR_SUCCESS != rk.SetValue(TEXT("xll_alert_level"), _level)) {
+			level = (DWORD)~0;
+			return;
+		}
+
+		level = _level;
+	}
+} xll_alert_;
 
 int 
 XLL_ALERT(const char* text, const char* caption, WORD level, UINT type, bool force)
 {
-	if ((xll_alert_level()&level) || force) {
+	if ((xll_alert_.level&level) || force) {
 		if (IDCANCEL == MessageBoxA(GetForegroundWindow(), text, caption, MB_OKCANCEL|type))
-			xll_alert_level(xll_alert_level() & ~level);
+			xll_alert_.set(xll_alert_.level & ~level);
 	}
 
-	return xll_alert_level();
+	return xll_alert_.level;
 }
 
 int 
