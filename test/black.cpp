aa6c081e6b3fa55e6cf5f61ef417a5d6368b2978
Keith A. Lewis <keithalewis@live.com>
2016-10-01 12:59:10 -0400

    Homework 2

--- test/black.cpp
+++ test/black.cpp
@@ -26,14 +26,50 @@ for (double x = -6; x <= 6; x += 0.1) {
 
 TEST_END
 
+AddIn xai_black_put_value(
+	Function(XLL_DOUBLE, L"?xll_black_put_value", L"PUT.VALUE")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward..")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike..")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility..")
+	.Arg(XLL_DOUBLE, L"t", L"is the time in years to expiration")
+	.Category(L"BLACK")
+	.FunctionHelp(L"The forward value of a put option using the Black model")
+	.Documentation()
+);
+double WINAPI xll_black_put_value(double f, double k, double s, double t)
+{
+#pragma XLLEXPORT
+	double value;
 
-//!!! Create an addin function called BLACK.PUT that calls black::put.
-//AddIn xai_black_put(...
+	try {
+		ensure (f > 0);
+		ensure (k > 0);
+		ensure (s > 0);
+		ensure (t > 0);
 
-//TEST_BEGIN(black_put)
+		value = black::put_value(f, s, k, t);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		value = std::numeric_limits<double>::quiet_NaN();
+	}
+
+	return value;
+}
+
+TEST_BEGIN(black_put)
 
 //!!! Write a test for xll_black_put using formula (27) of 
 // http://kalx.net/BlaSch1973.pdf for the values
-// f = x = 100, s = v = 0.2, k = c = 100, t = t* = 0.25, and r = 0.  
+// f = x = 100, s = v = 0.2, k = c = 100, t = t* = 0.25, and r = 0. 
 
-//TEST_END
+	double f = 100, k = 100, s = 0.2, t = 0.25, r = 0;
+	double d1 = (log(f/k) + (r + s*s/2)*t)/(s*sqrt(t));
+	double d2 = (log(f/k) + (r - s*s/2)*t)/(s*sqrt(t));
+	double N1 = prob::normal_cdf(-d1);
+	double N2 = prob::normal_cdf(-d2);
+	double v = -f*N1 + k*N2;
+
+	ensure (v == black::put_value(f, s, k, t));
+
+TEST_END
--- test/black.h
+++ test/black.h
@@ -14,23 +14,25 @@ namespace prob {
 
 namespace black {
 
-#pragma warning(push)
-#pragma warning(disable: 4100) // unreferenced formal parameter
 	// P(F <= k) where F = f exp(-s^2t/2 + s B_t)
+	// B_t/sqrt(t) <= (log k/f - s^2t/2)/s sqrt(t)
 	inline double P(double f, double s, double k, double t)
 	{
-		return 0; //!!! use appropriate formula
+		auto srt = s*sqrt(t);
+
+		return prob::normal_cdf(log(k/f)/srt + srt/2);
 	}
 
 	// P(F exp(s^2t) <= k) where F = f exp(-s^2t/2 + s B_t)
 	inline double P_(double f, double s, double k, double t)
 	{
-		return 0; //!!! use appropriate formula
+		auto srt = s*sqrt(t);
+
+		return prob::normal_cdf(log(k/f)/srt - srt/2);
 	}
-#pragma warning(pop)
 
 	// E max(k - F, 0) where F = f exp(-s^2t/2 + s B_t)
-	inline double put(double f, double s, double k, double t)
+	inline double put_value(double f, double s, double k, double t)
 	{
 		return k*P(f, s, k, t) - f*P_(f, s, k, t);
 	}
