7cbd30d2f9111a16e938f3189abc66fc8eeb352d
kal <kal@kalx.net>
2018-09-05 17:40:47 -0400

    sync

--- xll/addin.h
+++ xll/addin.h
@@ -1,16 +1,20 @@
 // addin.h - convenience wrapper for Excel add-ins
 // Copyright (c) KALX, LLC. All rights reserved. No warranty made.
 #pragma once
+#include <map>
 #include "auto.h"
 #include "args.h"
 
 namespace xll {
 	/// Manage the lifecycle of an Excel add-in.
 	class AddIn {
+        std::map<OPER12, Args> map_;
 	public:
 		/// Register and Unregister an add-in when Excel calls xlAutoOpen and xlAutoClose.
 		AddIn(const Args& args)
 		{
+            map_[args.FunctionText()] = args;
+
 			Auto<Open> ao([args]() { 
 				return args.Register().isNum();
 			});
@@ -18,7 +22,15 @@ namespace xll {
 				return args.Unregister();
 			});
 		}
-	};
+        auto begin() const
+        {
+            return map_.begin();
+        }
+        auto end() const
+        {
+            return map_.end();
+        }
+    };
 	using AddIn12 = AddIn;
 	using AddInX = AddIn;
 } // xll namespace
--- xll/doc.cpp
+++ xll/doc.cpp
@@ -26,6 +26,23 @@ namespace {
 	};
 }
 
+// guids based on strings
+inline std::wstring guid(const std::wstring& s)
+{
+    wchar_t buf[32+4+1];
+
+    std::hash<std::wstring> hash;
+    swprintf(buf, 36, L"%8x-0000-0000-0000-000000000000", hash(s));
+
+    return std::wstring(buf);
+}
+
+inline void make_function(const Args& args)
+{
+    OPER12 function = args.FunctionText();
+
+}
+
 static AddIn xai_make_doc(
     Macro(L"?xll_make_doc", L"XLL.MAKE.DOC")
 );
