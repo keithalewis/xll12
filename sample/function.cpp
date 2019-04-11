d7986e7626a4dfea6afe558234e7be06070d8f3b
kal <kal@kalx.net>
2019-04-11 17:00:54 -0400

    function type erasure for lambda and members

--- sample/function.cpp
+++ sample/function.cpp
@@ -5,7 +5,7 @@
 using namespace xll;
 
 AddIn xai_call_function(
-    Function(XLL_DOUBLE, L"?xll_call_function", L"CALL.FUNCTION")
+    Function(XLL_DOUBLE, L"?xll_call_function", L"FOO.CALL")
     .Arg(XLL_HANDLE, L"handle", L"is a handle to a function.")
     .Arg(XLL_DOUBLE, L"arg", L"is the function argument.")
     .FunctionHelp(L"Call a function")
@@ -31,6 +31,42 @@ HANDLEX WINAPI xll_foo_function()
 {
 #pragma XLLEXPORT
     xll::handle<std::function<double(double)>> h(new std::function{foo});
+
+    return h.get();
+}
+
+AddIn xai_foo_lambda(
+    Function(XLL_HANDLE, L"?xll_foo_lambda", L"FOO.LAMBDA")
+    .Uncalced()
+    .FunctionHelp(L"Return a handle to a lambda.")
+);
+HANDLEX WINAPI xll_foo_lambda()
+{
+#pragma XLLEXPORT
+    xll::handle<std::function<double(double)>> h(new std::function{[](double x) { return 3*x; }});
+
+    return h.get();
+}
+
+struct Foo {
+    double x;
+    Foo(double x) : x(x) {}
+    double operator()(double y)
+    {
+        return x*y;
+    }
+};
+AddIn xai_foo_member(
+    Function(XLL_HANDLE, L"?xll_foo_member", L"FOO.MEMBER")
+    .Uncalced()
+    .FunctionHelp(L"Return a handle to a member.")
+);
+HANDLEX WINAPI xll_foo_member()
+{
+#pragma XLLEXPORT
+    Foo bar(4);
+
+    xll::handle<std::function<double(double)>> h(new std::function{[&bar](double x) { return bar(x); }});
 
     return h.get();
 }
