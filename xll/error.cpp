e3924514b3eb3a549bbff61109253b8565c9b13c
kal <kal@kalx.net>
2019-11-27 14:36:44 -0500

    Factor out registry.h

--- xll/error.cpp
+++ xll/error.cpp
@@ -1,57 +1,7 @@
 #pragma warning(disable: 4996)
 #include <stdexcept>
 #include "xll.h"
-
-namespace Reg {
-	class CreateKey {
-		HKEY hkey;
-		DWORD disp;
-	public:
-		CreateKey(HKEY hKey, PCTSTR lpSubKey)
-		{
-			LSTATUS status = RegCreateKeyEx(hKey, lpSubKey, 0, 0, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, 0, &hkey, &disp);
-			if (status != ERROR_SUCCESS) {
-				throw std::runtime_error("CreateKey: RegCreateKeyEx failed");
-			}
-		}
-		CreateKey(const CreateKey&) = delete;
-		CreateKey& operator=(const CreateKey&) = delete;
-		~CreateKey()
-		{
-			RegCloseKey(hkey);
-		}
-		operator HKEY() const
-		{
-			return hkey;
-		}
-		DWORD disposition() const
-		{
-			return disp;
-		}
-		struct Proxy {
-			CreateKey& key;
-			PCTSTR value;
-			Proxy(CreateKey& key, PCTSTR value)
-				: key(key), value(value)
-			{ }
-			CreateKey& operator=(DWORD dword)
-			{
-				LSTATUS status = RegSetValueEx(key, value, 0, REG_DWORD, (const BYTE*)&dword, sizeof(DWORD));
-				if (ERROR_SUCCESS != status)
-				{
-					throw std::runtime_error("RegSetValueEx failed");
-				}
-
-				return key;
-			}
-			// operator=(string) etc
-		};
-		Proxy operator[](PCTSTR value)
-		{
-			return Proxy(*this, value);
-		}
-	};
-}
+#include "registry.h"
 
 class reg_alert_level {
 	DWORD value;
